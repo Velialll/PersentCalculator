dca5a46cf46d26c86bdb1c211cd1eeb1202f11f6
Velial <Velialxxx69@gmail.com>
2023-02-02 20:43:28 +0200

    Simple Percent Calculator

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,70 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("""
+                Select the calculator you want:
+                If you need Simple Percent Calculator press: 1
+                If you need Compound Interest Calculator press: 2""");
 
-        System.out.print("Enter the invested amount: ");
-        double investedAmount = sc.nextDouble();
+        String text = sc.nextLine();
 
-        System.out.print("Enter the interest rate: ");
-        double interestRate = sc.nextDouble();
+        if (text.equals("1")){
+            System.out.print("Enter the Initial Investment: ");
+            double initialInvestment = sc.nextDouble();
 
-        System.out.print("Enter the number of period: ");
-        double numberOfPeriod = sc.nextDouble();
+            System.out.print("Enter the Estimated Interest Rate: ");
+            double estimatedInterestRate = sc.nextDouble();
 
-        System.out.print("Result: ");
-        SimplePercentCalculator( investedAmount, interestRate, numberOfPeriod);
+            System.out.print("Enter the Length Of Time In Month: ");
+            double lengthOfTimeInMonth = sc.nextDouble();
+
+            System.out.print("Result: ");
+            SimplePercentCalculator(initialInvestment,
+                    estimatedInterestRate,
+                    lengthOfTimeInMonth);
+        }
+
+        if (text.equals("2")){
+
+            System.out.print("Enter the Initial Investment: ");
+            double initialInvestment1 = sc.nextDouble();
+
+            System.out.print("Enter the Estimated InterestRate: ");
+            double estimatedInterestRate1 = sc.nextDouble();
+
+            System.out.print("Enter the Monthly Contribution:");
+            double monthlyContribution1 = sc.nextDouble();
+
+            System.out.print("Enter the Length Of Time In Month: ");
+            double lengthOfTimeInMonth1 = sc.nextDouble();
+
+            System.out.print("Result: ");
+            CompoundInterestCalculator(initialInvestment1,
+                    estimatedInterestRate1,
+                    lengthOfTimeInMonth1,
+                    monthlyContribution1);
+        }
+
     }
-    static void SimplePercentCalculator(double investedAmount, double interestRate, double numberOfPeriod) {
 
-        interestRate = interestRate / 100;
-        numberOfPeriod = numberOfPeriod / 12;
-        double amountReceived = investedAmount * (1 + (interestRate * numberOfPeriod));
+    static void SimplePercentCalculator(double initialInvestment,
+                                        double estimatedInterestRate,
+                                        double lengthOfTimeInMonth) {
+        estimatedInterestRate /= 100.0;
+        lengthOfTimeInMonth /= 12.0;
+        double amountReceived = initialInvestment * (1.0 + estimatedInterestRate * lengthOfTimeInMonth);
         System.out.println(amountReceived);
+    }
+
+    static void CompoundInterestCalculator(double initialInvestment1,
+                                           double estimatedInterestRate1,
+                                           double monthlyContribution1,
+                                           double lengthOfTimeInMonth1){
+        estimatedInterestRate1 /= 100.0;
+        lengthOfTimeInMonth1 /= 12.0;
+        double amountReceived = initialInvestment1 * (1.0 + estimatedInterestRate1 * lengthOfTimeInMonth1);
+        System.out.println(amountReceived);
+
+
     }
 }
