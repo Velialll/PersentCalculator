9fffaaeb26f8a20a473536c2d8046dee254a3030
Velial <Velialxxx69@gmail.com>
2023-02-03 19:33:31 +0200

    TempFile

--- src/Temp.java
+++ src/Temp.java
@@ -16,16 +16,17 @@ public class Temp {
         System.out.println(A);
 
         A2 = P * (I * T);
-        System.out.println(A2);
+        System.out.println(A2+"\n\n\n\n\n\n");
         /**
          * 10000*(1-0.042)^5
          procent = 4.2 %
          balance * ( 1- (4.2%/100) ^ 5
          */
 
-        A = P * (1 + (I));
-        double operation = 10000 * (Math.pow(1 + 0.09, 12));
-
-        System.out.println(operation);
+        A = P * (1 + (I * T));
+        //A = P * (1 + (I)^?
+        double operation2 = 10000 * (Math.pow(1 + 0.09, T));
+        //10000 * ( 1 + 0.09)^2
+        System.out.println("operation2  " +operation2);
     }
 }
